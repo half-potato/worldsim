30f472cb75f51eeb1b3981b19fca9dd5552c8d62
Alex Mai <alexandertmai@gmail.com>
2018-01-23 15:51:31 -0800

    Stat system finished

--- src/Stat.py
+++ src/Stat.py
@@ -1,16 +1,43 @@
 import random
-from statTypes import types, STAT_NAMES
+from Types import statTypes, bonusTypes, STAT_NAMES
+
+class Bonus:
+    def __init__(self, name, desc=None, stats=None):
+        """
+        Parameters
+        ----------
+        name : str
+            Name of bonus. If stats is not defined, this is looked up in bonuses in types.py
+        desc : str
+            Description of bonus for flavor
+        stats : Dict : str -> int
+            Number to add to each stat
+        """
+        self.name = name
+        self.desc = desc
+        if stats:
+            self.stats = stats
+        else:
+            if not name in bonusTypes:
+                raise LookupError("Type %s not found in bonusTypes" % name)
+            bonus = bonusTypes[name]
+            self.stats = bonus["stats"]
+            self.desc = bonus["desc"]
+        if not self.desc:
+            self.desc = "Lazy DM"
 
 class Stat:
     """
     Usage:
     Looking up a stat:
     s = Stat(...)
-    s.str or s.stats["str"]
+    s.str
+    Check basestat with:
+    s.basestat["str"]
     """
     def __init__(self, *args, **kwargs):
         """
-        3 ways to init Stats: from args, from type of stats and bonus, and from ranges
+        3 ways to init Stats: from args, from type of stats and Modifier, and from ranges
         Args:
         Parameters
         ----------
@@ -24,72 +51,91 @@ class Stat:
         will : int
         luck : int
 
-        Type, Bonus:
+        Type, Modifier:
         Parameters
         ----------
         type : str
-            name of a type defined in types in statTypes.py
-        bonus : int
+            name of a type defined in types in types.py
+        Modifier : int
             constant added to every stat
 
         Ranges:
         Parameters
         ----------
         ranges : Dict : str -> int[2 or 3]
-            Must contain a value for every stat defined in STAT_NAMES in statTypes.py
+            Must contain a value for every stat defined in STAT_NAMES in types.py
             The int array is of the format [lowerBound, Upperbound, step] and is fed directly to random.randrange
         """
 
-        self.stats = {}
+        self.bonuses = {}
+        self.basestats = {}
         if len(args) == 9:
             # Init stats from args
             for i, v in enumerate(STAT_NAMES):
-                self.stats[v] = args[i]
+                self.basestats[v] = args[i]
         elif len(args) == 2:
-            # Init stats from type and bonus
-            if not args[0] in types:
-                raise LookupError("Type %s not found in types" % args[0])
-            ranges = types[args[0]]
-            # Add bonus
+            # Init basestats from type and Modifier
+            if not args[0] in statTypes:
+                raise LookupError("Type %s not found in statTypes" % args[0])
+            ranges = statTypes[args[0]]
+            # Add Modifier
             for k in ranges:
                 ranges[k] = [i+args[1] for i in ranges[k]]
             # Init using ranges
             self.__init__(ranges)
         elif len(args) == 1:
-            # Init stats from ranges. Can have extra stats.
-            # Check that all stats are in range
+            # Init basestats from ranges. Can have extra basestats.
+            # Check that all basestats are in range
             ranges = args[0]
             keys = ranges.keys()
             for k in STAT_NAMES:
                 if not k in keys:
                     raise LookupError("Attribute %s not found in initialization range for Stat" % k)
             for k, v in ranges.items():
-                self.stats[k] = random.randrange(*v)
+                self.basestats[k] = random.randrange(*v)
+
+    def addBonus(self, bonus):
+        self.bonuses[bonus.name] = bonus
+
+    def removeBonus(self, name):
+        del self.bonuses[name]
 
     def __getattr__(self, name):
-        if name == "stats":
+        """ Overriden to allow each stat to be looked up like a variable """
+        if name in ["basestats", "bonuses"]:
             return super(Stat, self).__getattr__(name)
-        elif name in self.stats:
-            return self.stats[name]
+        elif name in self.basestats:
+            basestat = self.basestats[name]
+            for n, b in self.bonuses.items():
+                if name in b.stats:
+                    basestat += b.stats[name]
+            return basestat
         else:
             return super(Stat, self).__getattr(name)
 
     def __setattr__(self, name, value):
-        if name == "stats":
+        """ Overriden to allow each stat to be looked up like a variable """
+        if name in ["basestats", "bonuses"]:
             super(Stat, self).__setattr__(name, value)
-        elif name in self.stats:
-            self.stats[name] = value
+        elif name in self.basestats:
+            self.basestats[name] = value
         else:
             super(Stat, self).__setattr__(name, value)
 
     def __str__(self):
+        """ Pretty print """
         s = ""
-        for k in self.stats:
-            s += "%s: %i\n" % (k, self.stats[k])
+        for k in self.basestats:
+            s += "%s: %i" % (k, self.basestats[k])
+            for n, b in self.bonuses.items():
+                if k in b.stats:
+                    s += " %+d from %s" % (b.stats[k], n)
+            s += "\n"
         return s
 
 s = Stat("even", 1)
 s.str = 10
+s.addBonus(Bonus("Bloodlust"))
 print(s)
 print(s.str)
-print(s.stats)
+print(s.basestats)
---
+++ src/Types.py
@@ -0,0 +1,64 @@
+STAT_NAMES = ["str", "con", "dex", "int", "cha", "wis", "per", "will", "luck"]
+
+statTypes = {
+}
+
+# Add a much of different variabilities
+
+def addFlatDistribution(name, high, low):
+    statTypes[name] = {}
+    for i in STAT_NAMES:
+        statTypes[name][i] = [low, high]
+
+def addFlatDistribution("var2", 9, 11) # Armies
+def addFlatDistribution("var4", 8, 12) # Similar people
+def addFlatDistribution("var6", 7, 13) # Humans
+
+def addFlatDistribution("smallAnimal", 3, 6)
+statTypes["smallAnimal"]["dex"] = [14, 16]
+
+def addFlatDistribution("animal", 4, 7)
+statTypes["animal"]["dex"] = [10, 12]
+statTypes["animal"]["int"] = [2, 4]
+
+def addFlatDistribution("largeAnimal", 4, 7)
+statTypes["largeAnimal"]["dex"] = [8, 10]
+statTypes["largeAnimal"]["con"] = [8, 10]
+statTypes["largeAnimal"]["str"] = [8, 10]
+
+bonusTypes = {
+    "Bloodlust": {
+        "stats": {
+            "str": 2,
+        },
+        "desc": "Loves hurting people",
+    },
+    "Kind": {
+        "stats": {
+            "cha": 2,
+            "str": -2,
+        },
+        "desc": "Kind to others, would never hurt a soul",
+    },
+    "I am very smart": {
+        "stats": {
+            "int": 1,
+            "cha": -2,
+        },
+        "desc": "Makes sure you know about every mistake you made"
+    },
+    "Careful": {
+        "stats": {
+            "per": 1,
+            "wis": 1,
+        },
+        "desc": "Takes the time to check everything is in order",
+    },
+    "Greedy": {
+        "stats": {
+            "cha": -1,
+            "luck": 1,
+        },
+        "desc": "The glimmer in their eye when they see your items makes you nervous.",
+    },
+}
